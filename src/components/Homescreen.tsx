b71f308e4430a93ce653c288777a7fe75a189166
Niharika <niharika@geekyants.com>
2024-01-25 17:01:20 +0530

    feat:add infinite scroll and refresh feature

--- src/components/Homescreen.tsx
+++ src/components/Homescreen.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import React, {useState, useCallback} from 'react';
+import {View, StyleSheet, Text, ActivityIndicator} from 'react-native';
 import {
   Image,
   ScrollView,
@@ -9,127 +9,149 @@ import {
   Icon,
   SearchIcon,
 } from '@gluestack-ui/themed';
-import feed from '../../utils/feed.json';
+import feeds from '../../utils/feed.json';
 import storyData from '../../utils/storyData.json';
 // @ts-ignore
 import {SliderBox} from 'react-native-image-slider-box';
+import {FlatList} from 'react-native';
+import {RefreshControl} from 'react-native';
+let feed = [...feeds];
 function Homescreen() {
+  const [visiblePost, setVisiblePost] = useState(2);
+  const [loadingMore, setLoadingMore] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+  const handleScroll = () => {
+    if (!loadingMore && visiblePost < feed.length) {
+      setLoadingMore(true);
+      setTimeout(() => {
+        setVisiblePost(prevCount => prevCount + 1);
+        setLoadingMore(false);
+      }, 1000);
+    } else {
+      console.log('End reached..');
+    }
+  };
+  const loader = () => (
+    <Box alignItems="center" padding={2} paddingBottom={14}>
+      <ActivityIndicator size="large" color="#808080" />
+    </Box>
+  );
+
+  const shuffleArray = (array: any[]) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    const newData = shuffleArray(feed);
+    setVisiblePost(2);
+    feed = newData;
+    setRefreshing(false);
+  }, []);
+
+  const renderHeader = () => (
+    <View style={styles.container}>
+      <Image
+        size="xs"
+        width={150}
+        marginTop={5}
+        marginLeft={-12}
+        source={{
+          uri: 'https://i.pinimg.com/originals/84/d8/61/84d861bd7bb0b5a2b4199abec253256c.png',
+        }}
+        alt="Logo"
+      />
+      <Image
+        size="xs"
+        width={12}
+        height={12}
+        position="absolute"
+        top={18}
+        left={121}
+        source={{
+          uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-MoCoHaAPT9kmJhBGb2pqu3E4gnbGP3w4Sw&usqp=CAU',
+        }}
+        alt="Arrow"
+      />
+
+      <Image
+        source={require('../../public/images/heart.png')}
+        width={24}
+        height={24}
+        position="absolute"
+        top={9}
+        right={60}
+        alt="heartIcon"
+      />
+      <Image
+        size="xs"
+        width={35}
+        height={55}
+        position="absolute"
+        top={-6}
+        right={5}
+        source={{
+          uri: 'https://st2.depositphotos.com/38069286/42112/v/450/depositphotos_421121214-stock-illustration-direct-messages-button-icon-isolated.jpg',
+        }}
+        alt="MessageIcon"
+      />
+    </View>
+  );
   return (
     <>
-      <ScrollView>
+      <>
         <View style={styles.container}>
-          <Image
-            size="xs"
-            width={150}
-            marginTop={5}
-            marginLeft={-12}
-            source={{
-              uri: 'https://i.pinimg.com/originals/84/d8/61/84d861bd7bb0b5a2b4199abec253256c.png',
-            }}
-            alt="Logo"
-          />
-          <Image
-            size="xs"
-            width={12}
-            height={12}
-            position="absolute"
-            top={18}
-            left={121}
-            source={{
-              uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-MoCoHaAPT9kmJhBGb2pqu3E4gnbGP3w4Sw&usqp=CAU',
-            }}
-            alt="Arrow"
-          />
-
-          <Image
-            source={require('../../public/images/heart.png')}
-            width={24}
-            height={24}
-            position="absolute"
-            top={9}
-            right={60}
-            alt="heartIcon"
-          />
-          <Image
-            size="xs"
-            width={35}
-            height={55}
-            position="absolute"
-            top={-6}
-            right={5}
-            source={{
-              uri: 'https://st2.depositphotos.com/38069286/42112/v/450/depositphotos_421121214-stock-illustration-direct-messages-button-icon-isolated.jpg',
-            }}
-            alt="MessageIcon"
-          />
-          <Box>
-            <ScrollView
-              horizontal
-              showsHorizontalScrollIndicator={false}
-              mt="$3">
-              <HStack space="sm">
-                {storyData.map((user, index) => (
-                  <Box key={index} ml="$2.5" alignItems="center">
-                    <Image
-                      alt="profilePicture"
-                      width={70}
-                      height={70}
-                      borderRadius={50}
-                      source={{uri: user.profilePicture}}
-                    />
-
-                    <Text style={styles.name}>{user.name}</Text>
-                  </Box>
-                ))}
-              </HStack>
-            </ScrollView>
-            <Divider my="$2" />
-          </Box>
-          <Box>
-            <Box py={5} position="relative">
-              {feed.map((data, index) => (
-                <>
-                  <>
-                    <View key={index}>
-                      <HStack alignItems="center">
-                        <Image
-                          source={{
-                            uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScaAsiURlbNIvNkNi5UCRzXStgONEKRH6emg&usqp=CAU',
-                          }}
-                          width={30}
-                          height={30}
-                          left={10}
-                          alt="UserImage"
-                          rounded={'$full'}
-                        />
-                        <Text left={16}>{data.Username}</Text>
-                        <Image
-                          source={{
-                            uri: 'https://static.vecteezy.com/system/resources/previews/021/190/333/original/more-vertical-three-dots-settings-filled-icon-in-transparent-background-basic-app-and-web-ui-bold-line-icon-eps10-free-vector.jpg',
-                          }}
-                          width={30}
-                          height={30}
-                          alt="Icon"
-                          position="absolute"
-                          right={6}
-                          bottom={2}
-                        />
-                      </HStack>
-                      <SliderBox
-                        images={data.Images.map(image => image.Url)}
-                        top={6}
-                        sliderBoxHeight={400}
-                        dotColor="#15ccf9"
-                        inactiveDotColor="grey"
-                        // eslint-disable-next-line react-native/no-inline-styles
-                        dotStyle={{
-                          top: 32,
-                          height: 5,
-                          width: 5,
+          <FlatList
+            ListHeaderComponent={renderHeader}
+            data={feed.slice(0, visiblePost)}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({item, index}) => (
+              <Box>
+                <Box py={5} position="relative">
+                  <View key={index}>
+                    <HStack alignItems="center">
+                      <Image
+                        source={{
+                          uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScaAsiURlbNIvNkNi5UCRzXStgONEKRH6emg&usqp=CAU',
                         }}
+                        width={30}
+                        height={30}
+                        left={10}
+                        alt="UserImage"
+                        rounded={'$full'}
                       />
-                    </View>
-                  </>
+                      <Text left={16}>{item.Username}</Text>
+                      <Image
+                        source={{
+                          uri: 'https://static.vecteezy.com/system/resources/previews/021/190/333/original/more-vertical-three-dots-settings-filled-icon-in-transparent-background-basic-app-and-web-ui-bold-line-icon-eps10-free-vector.jpg',
+                        }}
+                        width={30}
+                        height={30}
+                        alt="Icon"
+                        position="absolute"
+                        right={6}
+                        bottom={2}
+                      />
+                    </HStack>
+                    <SliderBox
+                      images={item.Images.map(image => image.Url)}
+                      top={6}
+                      sliderBoxHeight={400}
+                      dotColor="#15ccf9"
+                      inactiveDotColor="grey"
+                      // eslint-disable-next-line react-native/no-inline-styles
+                      dotStyle={{
+                        top: 35,
+                        height: 6,
+                        width: 6,
+                      }}
+                    />
+                  </View>
+
                   <HStack px={6} paddingTop={6} marginTop={10} gap={14}>
                     <Image
                       source={require('../../public/images/heart.png')}
@@ -169,20 +191,26 @@ function Homescreen() {
                   </HStack>
                   <Box px={'$1.5'} marginBottom={2} marginTop={2}>
                     <Text>
-                      <Text style={styles.Username}>{data.Username}</Text>
-                      <Text style={styles.Content}> {data.Caption}</Text>
+                      <Text style={styles.Username}>{item.Username}</Text>
+                      <Text style={styles.Content}> {item.Caption}</Text>
                     </Text>
                   </Box>
                   <HStack marginBottom={20}>
-                    <Text style={styles.Date}>{data.Date}</Text>
+                    <Text style={styles.Date}>{item.Date}</Text>
                   </HStack>
-                </>
-              ))}
-            </Box>
-          </Box>
+                </Box>
+              </Box>
+            )}
+            onEndReached={handleScroll}
+            onEndReachedThreshold={0.1}
+            ListFooterComponent={loadingMore ? loader : null}
+            refreshControl={
+              <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+            }
+          />
         </View>
-      </ScrollView>
-      <Box
+      </>
+      {/* <Box
         position="sticky"
         bottom={0}
         left={0}
@@ -196,7 +224,7 @@ function Homescreen() {
           <Icon as={SearchIcon} w="$7" h="$7" />
           <Icon as={SearchIcon} w="$7" h="$7" />
         </HStack>
-      </Box>
+      </Box> */}
     </>
   );
 }
@@ -222,6 +250,7 @@ const styles = StyleSheet.create({
   },
   container: {
     backgroundColor: 'white',
+    marginBottom: 10,
   },
   name: {
     marginTop: 7,
